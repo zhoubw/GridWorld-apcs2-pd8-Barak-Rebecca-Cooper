040075e20543fec98424184fb62031fa3011ee55
Merkuri <bz2118111@gmail.com>
2014-02-12 17:58:15 -0500

    Just putting this here

---
+++ Recursinator.java
@@ -0,0 +1,120 @@
+/*======================================
+  class Recursinator -- an refresher exercise in Java recursion
+  ======================================*/
+
+
+public class Recursinator {
+
+    /*******************************************************
+     * int fact(int n)
+     * @param n number whose factorial is to be computed
+     * precond: n >= 0
+     * eg: fact(5) -> 120
+     *******************************************************/
+    public static void fact( int n ) {
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+
+
+    /*******************************************************
+     * int fibRec(int n) returns nth term of Fibonacci sequence
+     *     Uses recursion.
+     * @param n specifies position of term in sequence.
+     * precond: 0th, 1st, 2nd terms are 0,1,1
+     * eg: fibRec(3) -> 2
+     *******************************************************/
+    public static int fibRec(int n) { 
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+
+
+    /*******************************************************
+     * int fibIt(int n) returns nth term of Fibonacci sequence
+     *     Uses iteratiion.
+     * @param n specifies position of term in sequence.
+     * precond: 0th, 1st, 2nd terms are 0,1,1
+     * eg: fibIt(3) -> 2
+     *******************************************************/
+    public static int fibIt(int n) { 
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+
+
+    /*******************************************************
+     * String fenceIt(int n) builds a fence with n fenceposts
+     * @param n number of |'s in return String
+     * precond: n >= 1
+     * eg: fenceIt(1) -> "|"
+     * eg: fenceIt(2) -> "|---|"
+     * eg: fenceIt(3) -> "|---|---|"
+     *******************************************************/
+    public static String fenceIt( int n ) { 
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+
+
+    /*******************************************************
+     * String fenceRec(int n) builds a fence with n fenceposts
+     * @param n number of |'s in return String
+     * precond: n >= 1
+     * eg: fenceRec(1) -> "|"
+     * eg: fenceRec(2) -> "|---|"
+     * eg: fenceRec(3) -> "|---|---|"
+     *******************************************************/
+    public static String fenceRec( int n ) { 
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+
+
+    /*******************************************************
+     * String commafyIt(int n) returns a String representation of
+     *        n with commas inserted where appropriate
+     * @param n number in need of commas
+     * precond: n >= 0
+     * eg: commafyIt(100) -> 100
+     * eg: commafyIt(1000) -> 1,000
+     * eg: commafyIt(10,000,000) -> 10,000,000
+     *******************************************************/
+    public static String commafyIt( int n ) { 
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+
+
+    /*******************************************************
+     * String commafyRec(int n) returns a String representation of
+     *        n with commas inserted where appropriate
+     * @param n number in need of commas
+     * precond: n >= 0
+     * eg: commafyRec(100) -> 100
+     * eg: commafyRec(1000) -> 1,000
+     * eg: commafyRec(10,000,000) -> 10,000,000
+     *******************************************************/
+    public static String commafyRec( int n ) { 
+	/*** YOUR IMPLEMENTATION HERE ***/
+    }
+
+
+
+
+    public static void main( String [] args ) {
+
+	/*============================================
+	  Main method for testing. Suggested use:
+	  Set up a list of test cases for each function 
+	  BEFORE you begin writing each method.
+	  Move top comment bar (----) down one line at a time
+	  when you're ready to test each line.
+	  ============================================*/
+
+	/*--------------------------------------------
+	System.out.println( "fact(0) -> " + fact(0) ); // 1
+	System.out.println( "fact(1) -> " + fact(1) ); // 1
+	System.out.println( "fact(2) -> " + fact(2) ); // 2
+	System.out.println( "fact(3) -> " + fact(3) ); // 6
+	System.out.println( "fact(4) -> " + fact(4) ); // 24
+	System.out.println( "fact(5) -> " + fact(5) ); // 120
+	  --------------------------------------------*/
+
+    }//end main
+
+}//end class Recursinator
